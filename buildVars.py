e01de1ab6a4137fbf8cc82f655535673bcc78d0d
bdorer1 <bernd.dorer@yahoo.de>
2015-04-18 22:18:43 +0200

    fixed syntax for builtvars.py

--- buildVars.py
+++ buildVars.py
@@ -9,22 +9,23 @@ _ = lambda x : x
 # Add-on information variables
 addon_info = {
 	# add-on Name
-	"addon-name" : "windows7magnifier",
+	"addon_name" : "windows7magnifier",
 	# Add-on description
 	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon-summary" : _("Windows 7 Magnifier integration for the NVDA GUI"),
+	"addon_summary" : _("Windows 7 Magnifier integration for the NVDA GUI"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on installation and add-on information
-	"addon-description" : _("""This addon integrates configuration options into the standard NVDA GUI. To access, select "Preferences", "Magnifier Options" from the NVDA GUI"""),
+	"addon_description" : _("""This addon integrates configuration options into the standard NVDA GUI. To access, select "Preferences", "Magnifier Options" from the NVDA GUI"""),
 	# version
-	"addon-version" : "1.2-dev",
+	"addon_version" : "1.2-dev",
 	# Author(s)
-	"addon-author" : "Dominic Canare <mail@greenlightgo.org> and nvda addons team",
+	"addon_author" : "Dominic Canare <mail@greenlightgo.org> and nvda addons team",
 	# URL for the add-on documentation support
-	"addon-url" : "http://www.greenlightgo.org/projects/nvda/"
+	"addon_url" : "http://www.greenlightgo.org/projects/nvda/",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 }
+
 
 import os.path
 
